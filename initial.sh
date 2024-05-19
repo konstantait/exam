04549afd0e786db2994eebabd4e1f74128f39bf6
konstantait <111305936+konstantait@users.noreply.github.com>
2024-05-19 12:36:34 +0300

    initial commit

--- initial.sh
+++ initial.sh
@@ -3,3 +3,7 @@
 su -
 apt-get update && apt-get -y install ssh git mc
 systemctl status sshd
+
+wget https://raw.githubusercontent.com/konstantait/exam/main/iptables.sh -O /root/iptables.sh 
+chmod +x /root/iptables.sh
+/etc/systemd/system
